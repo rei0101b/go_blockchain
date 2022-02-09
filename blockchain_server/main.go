d2fb7cb6dbbbcc51b0bf223f83f609827b1172e8
Rei <rei0101b@Rei.local>
2022-02-10 08:18:00 +0900

    webサーバーの立ち上げ

---
+++ blockchain_server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	fmt.Println("test")
+	// defaultの　ポート指定
+	port := flag.Uint("port", 5000, "TCP Port Number for Blockchain Server")
+	flag.Parse()
+	app := NewBlockChainServer(uint16(*port))
+	app.Run()
+}
