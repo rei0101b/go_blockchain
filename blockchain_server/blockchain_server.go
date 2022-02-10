61a3d476f7ee69913588af7ac8580c477914b7cd
Rei <rei0101b@Rei.local>
2022-02-10 09:27:17 +0900

    ブロックチェーン取得のAPI作成

--- blockchain_server/blockchain_server.go
+++ blockchain_server/blockchain_server.go
@@ -3,9 +3,13 @@ package main
 import (
 	"io"
 	"log"
+	"main/block"
+	"main/wallet"
 	"net/http"
 	"strconv"
 )
+
+var cache map[string]*block.Blockchain = make(map[string]*block.Blockchain)
 
 type BlockchainServer struct {
 	port uint16
@@ -19,11 +23,34 @@ func (bcs *BlockchainServer) Port() uint16 {
 	return bcs.port
 }
 
-func HelloWorld(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, World")
+func (bcs *BlockchainServer) GetBlockchain() *block.Blockchain {
+	bc, ok := cache["blockchain"]
+	if !ok {
+		minersWallet := wallet.NewWallet()
+		bc = block.NewBlockChain(minersWallet.BlockchainAddress(), bcs.port)
+		cache["blockchain"] = bc
+		bc.Print()
+		log.Printf("miner_private_key %v", minersWallet.PrivateKeyStr())
+		log.Printf("miner_public_key %v", minersWallet.PublicKeyStr())
+		log.Printf("miner_blockchain_address %v", minersWallet.BlockchainAddress())
+	}
+
+	return bc
+}
+
+func (bcs *BlockchainServer) GetChain(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		w.Header().Add("Content-Type", "application/json")
+		bc := bcs.GetBlockchain()
+		m, _ := bc.MarshalJSON()
+		io.WriteString(w, string(m[:]))
+	default:
+		log.Printf("ERROR: Invalid HTTP Method")
+	}
 }
 
 func (bsc *BlockchainServer) Run() {
-	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/", bsc.GetChain)
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bsc.port)), nil))
 }
